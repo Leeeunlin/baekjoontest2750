2c58b6901a5d6ab5d0c5a57b2d22a0865a94c6c0
Ellee <leeeunlin@icloud.com>
2022-02-21 14:30:33 +0900

    feat : 11654 test
    문자열 입력
    입력받은 문자열을 아스키로 변환

---
+++ 11654문제/11654.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("변환할 문자열을 입력하세요");
+char txt = Convert.ToChar(Console.ReadLine());
+int ascii = Convert.ToInt32(txt);
+Console.WriteLine("문자열" + txt + "를 ascii로 변환한 값은" + ascii +" 입니다.");
