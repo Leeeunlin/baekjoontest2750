df53a5953836d9cb2e98f02afd43db6cbdde665b
Ellee <leeeunlin@icloud.com>
2022-02-21 14:28:11 +0900

    feat : 2750 test
    array 생성
    숫자입력
    내림차순정렬
    입력 된 수 중복제거

---
+++ 2750문제/2750.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("몇개의 수를 입력 하시겠습니까?");
+int count = int.Parse(Console.ReadLine());
+int[] arr = new int[count];
+
+for (int i = 0; i < count; i++)
+{
+    Console.WriteLine("수를 입력 하세요.");
+    arr[i] = int.Parse(Console.ReadLine());
+}
+Array.Sort(arr);
+int[] arr2 = arr.Distinct().ToArray(); // 중복제거
+for (int j = 0; j < arr2.Length; j++)
+{
+    Console.WriteLine(arr2[j]);
+}
