d977fbc3772f14a1d7cc49aca00f3869a3d482f2
Ellee <leeeunlin@icloud.com>
2022-02-22 08:24:02 +0900

    feat :1193 test
    대각선 지그재그 규칙 확인
    홀수와 짝수일때 출력 확인

---
+++ 1193문제/1193.cs
@@ -0,0 +1,22 @@
+﻿string txt = Console.ReadLine();
+int i = int.Parse(txt);
+
+int count = 0;
+int sum = 0;
+
+while (i > sum) // 대각선 계산
+{
+    count++;
+    sum += count;
+}
+
+int arr = i - (sum - count); // 줄 계산
+
+if (count % 2 == 1) // 홀수일때
+{
+    Console.Write($"{count - arr + 1}/{arr}");
+}
+else // 짝수일때
+{
+    Console.Write($"{arr}/{count - arr + 1}");
+}
