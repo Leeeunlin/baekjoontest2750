d33c18689431d751008988549f0847e5f19f22a7
Ellee <leeeunlin@icloud.com>
2022-02-22 16:42:13 +0900

    feat :1003 test
    피보나치수열
    재귀쓰면 너무 느려짐

---
+++ 1003문제/1003.cs
@@ -0,0 +1,23 @@
+﻿int txt = int.Parse(Console.ReadLine());
+
+int[] fibo = new int[41];
+fibo[0] = 0;
+fibo[1] = 1;
+
+for (int i = 2; i < 41; i++)
+{
+    Console.WriteLine( fibo[i] = fibo[i - 1] + fibo[i - 2]);
+}
+
+for (int i = 0; i < txt; i++)
+{
+    int N = int.Parse(Console.ReadLine());
+    if (N == 0)
+    {
+        Console.WriteLine("1 0");
+    }
+    else
+    {
+        Console.WriteLine($"{fibo[N - 1]} {fibo[N]}");
+    }
+}
---
+++ 1110문제/1110.cs
@@ -0,0 +1,31 @@
+﻿string orgnum = Console.ReadLine();
+
+int count = 0;
+int addNum; // 10, 1의자리 계산값
+
+
+if (10 > int.Parse(orgnum)) // 10보다 작으면 0을 붙임
+{
+    orgnum = orgnum + 0;
+}
+string originalNum = orgnum;
+
+while (true)
+{
+    count++;
+    int a = int.Parse(orgnum) / 10; // 10자리
+    int b = int.Parse(orgnum) % 10; // 1자리
+    addNum = a + b;
+    Console.WriteLine(a + "+" + b + "=" + addNum);
+    Console.WriteLine(count + " 회 반복");
+    orgnum = b.ToString() + (addNum % 10).ToString();
+
+
+    if (int.Parse(orgnum) == int.Parse(originalNum))
+    {
+        Console.WriteLine(count + " 회로 반복 종료");
+        break;
+    }
+    // string 값으로 더해진 inputNum 과 변하지 않은 처음 입력값 originalNum을 비교하여 같으면 
+    // while문 탈출
+}
